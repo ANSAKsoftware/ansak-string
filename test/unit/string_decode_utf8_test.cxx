27dd2543548b86fa0eba725e86310fe9dd633052
Arthur N. Klassen <anklassen@onepost.net>
2016-06-11 17:34:21 -0700

    Completing the sub-moduling
    * CMakeLists.txt has boiler-plate in common with ansak-tools' CMakeLists.txt
    * copyright messages corrected in three test files

--- test/unit/string_decode_utf8_test.cxx
+++ test/unit/string_decode_utf8_test.cxx
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////
 //
-// Copyright (c) 2016, Arthur N. Klassen
+// Copyright (c) 2014, Arthur N. Klassen
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
